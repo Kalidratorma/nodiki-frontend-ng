0a79eed173dbfe9400c8588d974d3920e90bade5
kalidratorma <kalidratorma@gmail.com>
2025-04-15 20:23:35 +0400

    update angular to 19.2.7

--- src/app/modules/board/board-page/board-page.component.ts
+++ src/app/modules/board/board-page/board-page.component.ts
@@ -1,14 +1,52 @@
 import { Component } from '@angular/core';
+import { NgxGraphModule } from '@swimlane/ngx-graph';
+import { CommonModule } from '@angular/common';
+import { Node, Edge } from '@swimlane/ngx-graph';
 
 /**
- * Main board page component.
- * Displays user's workspace after login.
+ * Main board page with graph visualization.
  */
 @Component({
   selector: 'app-board-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgxGraphModule],
   templateUrl: './board-page.component.html',
   styleUrls: ['./board-page.component.scss']
 })
-export class BoardPageComponent {}
+export class BoardPageComponent {
+  /**
+   * List of graph nodes.
+   */
+  nodes: Node[] = [
+    {
+      id: '1',
+      label: 'Node 1',
+    },
+    {
+      id: '2',
+      label: 'Node 2',
+    },
+    {
+      id: '3',
+      label: 'Node 3',
+    }
+  ];
+
+  /**
+   * List of edges between nodes.
+   */
+  links: Edge[] = [
+    {
+      id: '1-2',
+      source: '1',
+      target: '2',
+      label: 'Link A'
+    },
+    {
+      id: '2-3',
+      source: '2',
+      target: '3',
+      label: 'Link B'
+    }
+  ];
+}
